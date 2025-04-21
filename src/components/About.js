501295e3a97a0178718773c07af7f14d2096e912
dilarabikmaz <112824705+dilarabikmaz@users.noreply.github.com>
2025-04-20 21:47:43 -0400

    Update bio and pp

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './About.css';
-import profilePic from '../assets/profile.jpg';
+import profilePic from '../assets/profile-pic.png';
 
 const About = () => {
   return (
@@ -8,8 +8,7 @@ const About = () => {
       <img src={profilePic} alt="Profile" className="profile-pic" />
       <div className="about-content">
         <h2>Hi, I'm Dilara!</h2>
-        <p>I'm a 3rd year Computer Science and Economics double-major at Yale passionate about simplifying life using technology. From analyzing urban mobility data for my research project to designing femtech products during my internship, I enjoy tackling all kinds of challenges that combine creativity, technology, and impact.</p>
-        <p>Some days you’ll catch me deep in code, other days obsessing over a product design, but every day I’m driven by ambition, curiosity, and a love for turning ideas into things people actually want to use. When I’m not building things, you’ll find me dancing, trying new foods, befriending every cat and dog I meet, or working to make life just a little better—one project at a time.
+        <p>I'm a 3rd year Computer Science and Economics double-major at Yale passionate about simplifying life using technology. </p>
 </p>
         <div className="social-links">
           <a href="https://www.linkedin.com/in/handan-dilara-b%C4%B1kmaz-7b4367174/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
