a253d11877be587e3db1f1a3e2f04566c61b6f2e
dilarabikmaz <112824705+dilarabikmaz@users.noreply.github.com>
2025-04-20 21:51:55 -0400

    Fix about.js bug

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,6 @@ const About = () => {
       <div className="about-content">
         <h2>Hi, I'm Dilara!</h2>
         <p>I'm a 3rd year Computer Science and Economics double-major at Yale passionate about simplifying life using technology. </p>
-</p>
         <div className="social-links">
           <a href="https://www.linkedin.com/in/handan-dilara-b%C4%B1kmaz-7b4367174/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           <a href="https://github.com/dilarabikmaz" target="_blank" rel="noopener noreferrer">GitHub</a>
